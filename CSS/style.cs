e065d230aa422e5fd9d25acc9e8d8cfa2cea6300
techiemithlesh <techie.mithlesh@gmail.com>
2022-12-12 17:49:47 +0530

    house consultancy services website

---
+++ CSS/style.cs
@@ -0,0 +1,45 @@
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'Roboto', sans-serif;
+}
+a {
+  text-decoration: none;
+}
+p {
+  font-family: 'Roboto';
+}
+.home {
+  background: #d4c2c2;
+}
+.contact,
+.portfolio {
+  width: 10rem;
+  padding: 5px;
+}
+button {
+  width: 10rem;
+  padding: 5px;
+}
+button:hover {
+  background: #000;
+  color: #fff;
+  transition: 2s all ease-in-out;
+}
+p {
+  font-family: 'Lato', sans-serif;
+}
+#Project-container {
+  background: #333;
+}
+.gallery {
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+}
+.gallery:hover {
+  transform: scale(1.4);
+}
---
+++ Js/index.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    autoplay: {
+        delay: 2000,
+    },
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
+  
